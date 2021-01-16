2ea92ca8cc1f6796c38ec25ba7008932f95d2e08
Robert Yang <58884455+Robear-y@users.noreply.github.com>
2021-01-16 11:51:37 -0500

    Revert "Added Jumping"
    This reverts commit 489fff8fbde1ce20fce54f50f318f614494aa269.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,39 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
-    public float speed;
-    public float jumpSpeed;
-    bool grounded = false;
-    public BoxCollider collider;
 
     // Update is called once per frame
     void Update(){
-
-        float horizontal = Input.GetAxis("Horizontal");
-        float jump = Input.GetAxis("Jump");
-
-        Vector2 move;
-
-        if(jump == 1 && grounded){
-            move = new Vector2(horizontal,jump*jumpSpeed);
-            grounded = false;
-        }
-        else {
-            move = new Vector2(horizontal,0);
-        }
         
-
-        
-        transform.Translate(move*Time.deltaTime*speed);
     }
 
     void FixedUpdate (){
         
-    }
-
-    void OnCollisionEnter (Collision collision){
-        if(collision.gameObject.layer == 8){
-            grounded = true;
-        }
     }
 }
